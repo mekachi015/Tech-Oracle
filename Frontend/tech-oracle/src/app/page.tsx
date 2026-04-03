c180f40bc1de06e58d8d300f01db308738d83d69
mekachi015 <katlegomakoti@gmail.com>
2026-04-03 15:35:37 +0200

    hero section added

--- Frontend/tech-oracle/src/app/page.tsx
+++ Frontend/tech-oracle/src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 import UserForm from "@/components/user-form";
+import HeroSection from "@/components/HeroSection";
 import ClientWrapper from "./clientWrapper";
 
 export default function Home() {
   return (
-    <ClientWrapper>
-      <UserForm />
-    </ClientWrapper>
+    <>
+      <HeroSection formSectionId="repair-form" />
+      <div id="repair-form">
+        <ClientWrapper>
+          <UserForm />
+        </ClientWrapper>
+      </div>
+    </>
   );
 }
